9199680fe6ccd73493c8b48fc8e8bc43088a34e1
revchester <akuma.tenshi007@gmail.com>
2025-01-27 12:19:12 +0700

    Add files via upload

--- brush/js/script.js
+++ brush/js/script.js
@@ -201,3 +201,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
